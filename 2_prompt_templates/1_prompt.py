a8e01135484c2ae273d9425d720de9a64f4c2b1d
dotmebhanu <dotmebhanu@gmail.com>
2026-03-24 11:58:05 +0530

    prompt template done

---
+++ 2_prompt_templates/1_prompt.py
@@ -0,0 +1,30 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+llm = ChatGroq(model="llama-3.1-8b-instant")
+
+# template = "Write a {tone} email to {company} expressing interest in the {position} position, mentioning {skill} as a key strength. Keep it to 4 lines max"
+
+# prompt_template = ChatPromptTemplate.from_template(template)
+
+# prompt =  prompt_template.invoke({
+#     "tone": "energetic", 
+#     "company": "samsung", 
+#     "position": "AI Engineer", 
+#     "skill": "AI"
+# })
+
+# Example 2: Prompt with System and Human Messages (Using Tuples)
+messages = [
+    ("system", "You are a comedian who tells jokes about {topic}."),
+    ("human", "Tell me {joke_count} jokes."),
+]
+
+prompt_template = ChatPromptTemplate.from_messages(messages)
+prompt = prompt_template.invoke({"topic": "lawyers", "joke_count": 3})
+result = llm.invoke(prompt)
+print(result.content)
